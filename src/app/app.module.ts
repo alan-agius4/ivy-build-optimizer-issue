f8138b0dec315fcd98277ae23c2cf417af38d059
Alan Agius <alan.agius@betssongroup.com>
2018-12-21 10:36:05 +0100

    add boostrapping code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -7,7 +7,9 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
